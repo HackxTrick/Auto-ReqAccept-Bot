35f102d9aeb2d890ffa2aab6110733f8aef3c085
HackxTrick <153048345+HackxTrick@users.noreply.github.com>
2024-06-23 15:08:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import asyncio, datetime, time
 
 
 ACCEPTED_TEXT = "Hey {user}\n\nYour Request For {chat} Is Accepted ✅"
-START_TEXT = "Hai {}\n\nI am Auto Request Accept Bot With Working For All Channel. Add Me In Your Channel To Use"
+START_TEXT = "Hai {}\n\nI am Auto Request Accept Bot With Working For All Channels And Groups. Add Me In Your Channel Or Group To Use"
 
 API_ID = int(env.get('API_ID'))
 API_HASH = env.get('API_HASH')
@@ -27,8 +27,10 @@ async def start_handler(c, m):
     user_id = m.from_user.id
     if not await Data.find_one({'id': user_id}): await Data.insert_one({'id': user_id})
     button = [[        
-        InlineKeyboardButton('Updates', url='https://t.me/mkn_bots_updates'),
-        InlineKeyboardButton('Support', url='https://t.me/MKN_BOTZ_DISCUSSION_GROUP')
+        InlineKeyboardButton('UPDATES', url='https://t.me/GAURAV_BOTS'),
+        InlineKeyboardButton('OWNER⚡', url='https://t.me/ANIME_TIMETV'),
+        InlineKeyboardButton('SUPPORT', url='https://t.me/INFINITE_EMPIRE')
+           
     ]]
     return await m.reply_text(text=START_TEXT.format(m.from_user.mention), disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(button))
           
